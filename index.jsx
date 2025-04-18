e14a45066c76e07de6073d8de9716cbc89092a2d
Dhruba <gdhruba748+github@gmail.com>
2025-04-19 04:14:02 +0530

    setup

---
+++ index.jsx
@@ -0,0 +1,8 @@
+// src/index.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => <h1>Hello from React + Parcel!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
