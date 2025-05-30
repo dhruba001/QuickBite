c5b77997b9256d077dd0db687ef5452bce9ea633
Dhruba <gdhruba748+github@gmail.com>
2025-05-30 15:15:11 +0530

    minor changes

--- App.js
+++ App.js
@@ -2,7 +2,3 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const App = () => <h1>Hello from React + Parcel!</h1>;
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
