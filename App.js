3e67f2cf2d702f0d35f374e7707241257ad5f1a0
Dhruba <gdhruba748+github@gmail.com>
2025-07-04 18:21:13 +0530

    Made a basic header for the app

--- App.js
+++ App.js
@@ -1,4 +1,39 @@
-// src/index.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.shutterstock.com/image-vector/food-delivery-black-color-label-260nw-788158552.jpg"
+          alt="QuickBite_LOGO"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About US</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
