a63fc0622717e06c3ccf67f1a6a027e33f73a90b
Dhruba <gdhruba748+github@gmail.com>
2025-07-05 20:22:35 +0530

    destructed for uniformity

--- App.js
+++ App.js
@@ -30,6 +30,8 @@ const styleCard = {
 
 const ResturantCard = (props) => {
   const { resData } = props;
+  const { cloudinaryImageId, name, cuisines, avgRating, sla, costForTwo } =
+    resData?.info;
   //console.log(props);
   return (
     <div className="res-card" style={styleCard}>
@@ -38,14 +40,14 @@ const ResturantCard = (props) => {
         alt="res-logo"
         src={
           "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
-          resData.info.cloudinaryImageId
+          cloudinaryImageId
         }
       />
-      <h3>{resData.info.name}</h3>
-      <h4>{resData.info.cuisines.join(", ")}</h4>
-      <h4>{resData.info.avgRating}</h4>
-      <h4>{resData.info.sla.slaString}</h4>
-      <h4>{resData.info.costForTwo}</h4>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{sla.slaString}</h4>
+      <h4>{costForTwo}</h4>
     </div>
   );
 };
