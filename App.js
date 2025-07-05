758afb32dcf8a4e0a4d9243ab268583518b20522
Dhruba <gdhruba748+github@gmail.com>
2025-07-05 17:47:54 +0530

    taking dynamic values from swiggy api

--- App.js
+++ App.js
@@ -29,21 +29,91 @@ const styleCard = {
 };
 
 const ResturantCard = (props) => {
-  const { resName, cuisine } = props;
+  const { resData } = props;
   //console.log(props);
   return (
     <div className="res-card" style={styleCard}>
       <img
         className="res-logo"
         alt="res-logo"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2025/6/6/f6c81999-57b6-4eb7-ac66-dc1c3485f2e4_6ba607a5-1751-462a-a73d-68ae91ade469.jpg"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          resData.info.cloudinaryImageId
+        }
       />
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
-      <h4>4.4 stars</h4>
-      <h4>38 mins</h4>
+      <h3>{resData.info.name}</h3>
+      <h4>{resData.info.cuisine}</h4>
+      <h4>{resData.info.avgRating}</h4>
+      <h4>{resData.info.sla.slaString}</h4>
     </div>
   );
+};
+
+const resObj = {
+  "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+  info: {
+    id: "8032",
+    name: "Burger King",
+    cloudinaryImageId:
+      "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/f448a87b-da49-4da4-9fa4-d9ed0436bbe8_8032.jpg",
+    locality: "RMV 2nd Stage",
+    areaName: "Sanjaynagar",
+    costForTwo: "₹350 for two",
+    cuisines: ["Burgers", "American"],
+    avgRating: 4.3,
+    parentId: "166",
+    avgRatingString: "4.3",
+    totalRatingsString: "84K+",
+    sla: {
+      deliveryTime: 40,
+      lastMileTravel: 9,
+      serviceability: "SERVICEABLE",
+      slaString: "35-40 mins",
+      lastMileTravelString: "9.0 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2025-07-06 05:00:00",
+      opened: true,
+    },
+    badges: {},
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {},
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "ITEMS",
+      subHeader: "AT ₹29",
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+    externalRatings: {
+      aggregatedRating: {
+        rating: "--",
+      },
+    },
+    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+  },
 };
 
 const Body = () => {
@@ -51,11 +121,8 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <ResturantCard
-          resName="Meghna Foods"
-          cuisine="Biriyani,North Indian, Asian"
-        />
-        <ResturantCard resName="KFC" cuisine="Burger, Fast Food" />
+        <ResturantCard resData={resObj} />
+        <ResturantCard resData={resObj} />
       </div>
     </div>
   );
