194ca3921da5d45793d283f5d52c279a4c7e6ce1
Dhruba <gdhruba748+github@gmail.com>
2025-07-05 18:22:16 +0530

    made app data dynamic from list data

--- App.js
+++ App.js
@@ -42,87 +42,397 @@ const ResturantCard = (props) => {
         }
       />
       <h3>{resData.info.name}</h3>
-      <h4>{resData.info.cuisine}</h4>
+      <h4>{resData.info.cuisines.join(", ")}</h4>
       <h4>{resData.info.avgRating}</h4>
       <h4>{resData.info.sla.slaString}</h4>
+      <h4>{resData.info.costForTwo}</h4>
     </div>
   );
 };
 
-const resObj = {
-  "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
-  info: {
-    id: "8032",
-    name: "Burger King",
-    cloudinaryImageId:
-      "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/f448a87b-da49-4da4-9fa4-d9ed0436bbe8_8032.jpg",
-    locality: "RMV 2nd Stage",
-    areaName: "Sanjaynagar",
-    costForTwo: "₹350 for two",
-    cuisines: ["Burgers", "American"],
-    avgRating: 4.3,
-    parentId: "166",
-    avgRatingString: "4.3",
-    totalRatingsString: "84K+",
-    sla: {
-      deliveryTime: 40,
-      lastMileTravel: 9,
-      serviceability: "SERVICEABLE",
-      slaString: "35-40 mins",
-      lastMileTravelString: "9.0 km",
-      iconType: "ICON_TYPE_EMPTY",
-    },
-    availability: {
-      nextCloseTime: "2025-07-06 05:00:00",
-      opened: true,
-    },
-    badges: {},
-    isOpen: true,
-    type: "F",
-    badgesV2: {
-      entityBadges: {
-        imageBased: {},
-        textBased: {},
-        textExtendedBadges: {},
+// const resObj = {
+//   "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+//   info: {
+//     id: "8032",
+//     name: "Burger King",
+//     cloudinaryImageId:
+//       "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/f448a87b-da49-4da4-9fa4-d9ed0436bbe8_8032.jpg",
+//     locality: "RMV 2nd Stage",
+//     areaName: "Sanjaynagar",
+//     costForTwo: "₹350 for two",
+//     cuisines: ["Burgers", "American"],
+//     avgRating: 4.3,
+//     parentId: "166",
+//     avgRatingString: "4.3",
+//     totalRatingsString: "84K+",
+//     sla: {
+//       deliveryTime: 40,
+//       lastMileTravel: 9,
+//       serviceability: "SERVICEABLE",
+//       slaString: "35-40 mins",
+//       lastMileTravelString: "9.0 km",
+//       iconType: "ICON_TYPE_EMPTY",
+//     },
+//     availability: {
+//       nextCloseTime: "2025-07-06 05:00:00",
+//       opened: true,
+//     },
+//     badges: {},
+//     isOpen: true,
+//     type: "F",
+//     badgesV2: {
+//       entityBadges: {
+//         imageBased: {},
+//         textBased: {},
+//         textExtendedBadges: {},
+//       },
+//     },
+//     aggregatedDiscountInfoV3: {
+//       header: "ITEMS",
+//       subHeader: "AT ₹29",
+//     },
+//     orderabilityCommunication: {
+//       title: {},
+//       subTitle: {},
+//       message: {},
+//       customIcon: {},
+//     },
+//     differentiatedUi: {
+//       displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//       differentiatedUiMediaDetails: {
+//         mediaType: "ADS_MEDIA_ENUM_IMAGE",
+//         lottie: {},
+//         video: {},
+//       },
+//     },
+//     reviewsSummary: {},
+//     displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//     restaurantOfferPresentationInfo: {},
+//     externalRatings: {
+//       aggregatedRating: {
+//         rating: "--",
+//       },
+//     },
+//     ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+//   },
+// };
+
+const resList = [
+  {
+    info: {
+      id: "8032",
+      name: "Burger King",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/f448a87b-da49-4da4-9fa4-d9ed0436bbe8_8032.jpg",
+      locality: "RMV 2nd Stage",
+      areaName: "Sanjaynagar",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.3,
+      parentId: "166",
+      avgRatingString: "4.3",
+      totalRatingsString: "84K+",
+      sla: {
+        deliveryTime: 40,
+        lastMileTravel: 9,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "9.0 km",
+        iconType: "ICON_TYPE_EMPTY",
       },
-    },
-    aggregatedDiscountInfoV3: {
-      header: "ITEMS",
-      subHeader: "AT ₹29",
-    },
-    orderabilityCommunication: {
-      title: {},
-      subTitle: {},
-      message: {},
-      customIcon: {},
-    },
-    differentiatedUi: {
-      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-      differentiatedUiMediaDetails: {
-        mediaType: "ADS_MEDIA_ENUM_IMAGE",
-        lottie: {},
-        video: {},
+      availability: {
+        nextCloseTime: "2025-07-06 05:00:00",
+        opened: true,
       },
-    },
-    reviewsSummary: {},
-    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-    restaurantOfferPresentationInfo: {},
-    externalRatings: {
-      aggregatedRating: {
-        rating: "--",
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
       },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹29",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
     },
-    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
   },
-};
+  {
+    info: {
+      id: "672074",
+      name: "Subway",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/12/8f9a90a5-4f89-4234-b4ed-a840580dbe52_672074.jpg",
+      locality: "Ashok Nagar",
+      areaName: "Sobha Mall",
+      costForTwo: "₹350 for two",
+      cuisines: ["sandwich", "Salads", "wrap", "Healthy Food"],
+      avgRating: 4.4,
+      parentId: "2",
+      avgRatingString: "4.4",
+      totalRatingsString: "1.9K+",
+      sla: {
+        deliveryTime: 18,
+        lastMileTravel: 1.6,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "1.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-07-06 03:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "bolt/big%20rx%20listing%2015%20mins%20.png",
+            description: "bolt!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "bolt!",
+                  imageId: "bolt/big%20rx%20listing%2015%20mins%20.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹169",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "901258",
+      name: "Dunkin' - Donuts & Coffee",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/16/15b7ebb9-05eb-49c5-a309-17cdcf101316_901258.jpg",
+      locality: "Shivajinagara",
+      areaName: "Central Bangalore",
+      costForTwo: "₹250 for two",
+      cuisines: ["Desserts", "Cafe", "Bakery", "Beverages", "Fast Food"],
+      avgRating: 4.4,
+      parentId: "2276",
+      avgRatingString: "4.4",
+      totalRatingsString: "395",
+      sla: {
+        deliveryTime: 18,
+        lastMileTravel: 3,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "3.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-07-06 00:30:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹149",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "533773",
+      name: "Third Wave Coffee",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/23/85eb067b-1de2-434b-8c2a-e833c94ab489_533773.jpg",
+      locality: "Lavelle Road",
+      areaName: "Lavelle Road",
+      costForTwo: "₹400 for two",
+      cuisines: ["Beverages", "Bakery", "Continental"],
+      avgRating: 4.6,
+      parentId: "274773",
+      avgRatingString: "4.6",
+      totalRatingsString: "650",
+      sla: {
+        deliveryTime: 17,
+        lastMileTravel: 0.9,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "0.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-07-06 00:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "bolt/big%20RX%20listing%2018px.png",
+            description: "bolt!",
+          },
+          {
+            imageId: "Rxawards/_CATEGORY-Cafe%20&%20Chai.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "bolt!",
+                  imageId: "bolt/big%20RX%20listing%2018px.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Cafe%20&%20Chai.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹115",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.6",
+          ratingCount: "747",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+];
 
 const Body = () => {
   return (
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <ResturantCard resData={resObj} />
-        <ResturantCard resData={resObj} />
+        {resList.map((restaurant) => (
+          <ResturantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
