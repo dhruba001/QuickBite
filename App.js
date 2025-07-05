2570b312346e94c61e920c8d5d270ac1328caedb
Dhruba <gdhruba748+github@gmail.com>
2025-07-05 06:45:01 +0530

    resturant and body component

--- App.js
+++ App.js
@@ -24,7 +24,36 @@ const Header = () => {
   );
 };
 
-const Body = () => {};
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+const ResturantCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2025/6/6/f6c81999-57b6-4eb7-ac66-dc1c3485f2e4_6ba607a5-1751-462a-a73d-68ae91ade469.jpg"
+      />
+      <h3>Meghna Foods</h3>
+      <h4>Biriyani,North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 mins</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <ResturantCard />
+      </div>
+    </div>
+  );
+};
 
 const AppLayout = () => {
   return (
