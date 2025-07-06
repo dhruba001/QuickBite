38097b73f478cd8262aa7b4d6a6e8cada448abd1
Dhruba <gdhruba748+github@gmail.com>
2025-07-06 15:36:20 +0530

    making a reset button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,39 +1,38 @@
 import ResturantCard from "./ResturantCard";
 import { useState } from "react";
-//import { resList } from "../utils/mockData";
+import { resList } from "../utils/mockData";
 
 const Body = () => {
-  let listOfRestaurants = [
-    {
-      info: {
-        id: "8032",
-        name: "Burger King",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/f448a87b-da49-4da4-9fa4-d9ed0436bbe8_8032.jpg",
-        cuisines: ["Burgers", "American"],
-        avgRating: 4.3,
-        sla: {
-          slaString: "35-40 mins",
-        },
-      },
-    },
-    {
-      info: {
-        id: "8033",
-        name: "Dominos",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/f448a87b-da49-4da4-9fa4-d9ed0436bbe8_8032.jpg",
-        cuisines: ["Burgers", "American"],
-        avgRating: 4.9,
-        sla: {
-          slaString: "35-40 mins",
-        },
-      },
-    },
-  ];
+  // let listOfRestaurants = [
+  //   {
+  //     info: {
+  //       id: "8032",
+  //       name: "Burger King",
+  //       cloudinaryImageId:
+  //         "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/f448a87b-da49-4da4-9fa4-d9ed0436bbe8_8032.jpg",
+  //       cuisines: ["Burgers", "American"],
+  //       avgRating: 4.3,
+  //       sla: {
+  //         slaString: "35-40 mins",
+  //       },
+  //     },
+  //   },
+  //   {
+  //     info: {
+  //       id: "8033",
+  //       name: "Dominos",
+  //       cloudinaryImageId:
+  //         "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/f448a87b-da49-4da4-9fa4-d9ed0436bbe8_8032.jpg",
+  //       cuisines: ["Burgers", "American"],
+  //       avgRating: 4.9,
+  //       sla: {
+  //         slaString: "35-40 mins",
+  //       },
+  //     },
+  //   },
+  // ];
 
-  const [listOfRestaurants1, setListOfRestaurants1] =
-    useState(listOfRestaurants);
+  const [listOfRestaurants1, setListOfRestaurants1] = useState(resList);
 
   return (
     <div className="body">
@@ -49,6 +48,13 @@ const Body = () => {
           }}
         >
           Top rated restaurant
+        </button>
+        <button
+          onClick={() => {
+            setListOfRestaurants1(resList);
+          }}
+        >
+          Reset
         </button>
       </div>
       <div className="res-container">
