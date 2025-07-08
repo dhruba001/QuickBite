03ee0a1e8cafe1f3271df1133b93ea27442caee4
Dhruba <gdhruba748+github@gmail.com>
2025-07-08 17:10:25 +0530

    debugging the search function

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,7 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
   const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
@@ -15,6 +16,9 @@ const Body = () => {
     const data = await fetch(SWIGGY_API_LIVE);
     const json = await data.json();
     setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
@@ -38,7 +42,7 @@ const Body = () => {
               const filteredList = listOfRestaurants.filter((res) =>
                 res.info.name.toLowerCase().includes(searchText.toLowerCase())
               );
-              setListOfRestaurants(filteredList);
+              setFilteredRestaurant(filteredList);
             }}
           >
             Search
@@ -50,14 +54,14 @@ const Body = () => {
             const filterdListOfRestaurants = listOfRestaurants.filter(
               (res) => res.info.avgRating > 4.3
             );
-            setListOfRestaurants(filterdListOfRestaurants);
+            setFilteredRestaurant(filterdListOfRestaurants);
           }}
         >
           Top rated restaurant
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <ResturantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
