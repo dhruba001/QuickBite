5ba7582640fc45c380c98d730bbb1f0294142303
Dhruba <gdhruba748+github@gmail.com>
2025-07-14 16:26:13 +0530

    removed unused import and added contact component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
 import About from "./components/About.js";
+import Contact from "./components/Contact.js";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
@@ -22,6 +22,10 @@ const appRouter = createBrowserRouter([
   {
     path: "/about",
     element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
   },
 ]);
 
