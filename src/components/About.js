ace8d16433a05add1f63e99f7ee784be44274ce0
Dhruba <gdhruba748+github@gmail.com>
2025-07-16 07:58:24 +0530

    about us page is modified

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
+import User from "./User";
+
 const About = () => {
   return (
     <div>
       <h1>About Us</h1>
       <h2>This is about my web app</h2>
+      <User />
     </div>
   );
 };
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+  return (
+    <div className="user-card">
+      <h2>Name: Dhruba</h2>
+      <h3>Location: Bangalore</h3>
+      <h4>Contact: @dhruba_001 </h4>
+    </div>
+  );
+};
+
+export default User;
