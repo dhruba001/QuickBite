4d478fc1b0d05dd43ea0676a45b25ddf2c12f59a
Dhruba <gdhruba748+github@gmail.com>
2025-07-21 11:23:17 +0530

    using context api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import UserContext from "../utils/UserContext";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
   const onlineStatus = useOnlineStatus();
+  const { loggedInUser } = useContext(UserContext);
 
   return (
     <div className="flex justify-between bg-pink-200 shadow-lg">
@@ -14,25 +16,25 @@ const Header = () => {
       </div>
       <div className="flex items-center">
         <ul className="flex p-4 m-4">
-          <li className="px-4 text-2xl">
+          <li className="px-4 text-xl">
             Online Status :{onlineStatus ? "✅" : "🔴"}
           </li>
-          <li className="text-2xl">
+          <li className="text-xl">
             <Link to="/">Home</Link>
           </li>
-          <li className="px-4 text-2xl">
+          <li className="px-4 text-xl">
             <Link to="/about">AboutUs</Link>
           </li>
-          <li className="px-4 text-2xl">
+          <li className="px-4 text-xl">
             <Link to="/contact">Contact</Link>
           </li>
-          <li className="px-4 text-2xl">
+          <li className="px-4 text-xl">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li className="px-4 text-2xl">Cart</li>
-          <li className="px-4 text-2xlss">
+          <li className="px-4 text-xl">Cart</li>
+          <li className="px-4">
             <button
-              className="login"
+              className="login text-xl"
               onClick={() => {
                 if (btnName === "Login") setBtnName("Logout");
                 else setBtnName("Login");
@@ -41,6 +43,7 @@ const Header = () => {
               {btnName}
             </button>
           </li>
+          <li className="px-4 font-bold text-xl">{loggedInUser}</li>
         </ul>
       </div>
     </div>
