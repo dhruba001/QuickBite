62d7cf3eb99caf5b70e858feffea9be3d637d1ae
Dhruba <gdhruba748+github@gmail.com>
2025-07-23 21:31:39 +0530

    built cart functionality

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import ItemList from "./ItemList";
+import { clearCart } from "../utils/Slice/cartSlice";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const cartItems = useSelector((store) => store.cart.items);
+
+  const dispatch = useDispatch();
+
+  const handelClearCart = () => {
+    dispatch(clearCart());
+  };
+
+  return (
+    <div className="text-center m-5 p-5">
+      <h1 className="text-4xl font-bold">Cart</h1>
+      <button
+        className="p-2 m-2 bg-black text-white rounded-lg"
+        onClick={handelClearCart}
+      >
+        Clear Cart
+      </button>
+      {cartItems.length === 0 && <h1>Cart is empty, please add items !!</h1>}
+      <div className="w-9/12 m-auto">
+        <ItemList items={cartItems} />
+      </div>
+    </div>
+  );
 };
 export default Cart;
