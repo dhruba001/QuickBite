a71650072800d173ed1aaf2efd9caac83d825cef
Dhruba <gdhruba748+github@gmail.com>
2025-07-30 15:27:17 +0530

    Test Cases for header.js

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import Header from "../Header";
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
+
+test("Should load header component with a login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+});
