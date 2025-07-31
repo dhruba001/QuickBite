7a60fa402d0c40f7f5f96e155803f4dc01004c30
Dhruba <gdhruba748+github@gmail.com>
2025-07-31 12:13:48 +0530

    mock data for restaurant card test cases and test case file

---
+++ src/components/__tests__/RestaurantCard.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import RestaurantCard from "../ResturantCard";
+import MOCK_DATA from "../mocks/resCardMock.json";
+import "@testing-library/jest-dom";
+
+it("Should render RestaurantCard component with props Data", () => {
+  render(<RestaurantCard resData={MOCK_DATA} />);
+  const name = screen.getByText("Chinese Wok");
+  expect(name).toBeInTheDocument();
+});
