f12c457e39b0eeb7684a9774fc646089e06f2b60
Dhruba <gdhruba748+github@gmail.com>
2025-08-01 12:54:32 +0530

    test cases for add to cart

---
+++ src/components/__tests__/Cart.test.js
@@ -0,0 +1,40 @@
+import { act } from "react";
+import RestaurantMenu from "../RestaurantMenu";
+import Header from "../Header";
+import MOCK_DATA from "../mocks/mockResMenu.json";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+global.fetch = jest.fn(() => {
+  return Promise.resolve({
+    json: () => {
+      return Promise.resolve(MOCK_DATA);
+    },
+  });
+});
+
+it("should load restraurant menu component", async () => {
+  await act(async () =>
+    render(
+      <BrowserRouter>
+        <Provider store={appStore}>
+          <Header />
+          <RestaurantMenu />
+        </Provider>
+      </BrowserRouter>
+    )
+  );
+
+  // open recommended accordion tab and check for 14 menu items
+  const accordionHeader = screen.getByText("Recommended (14)");
+  fireEvent.click(accordionHeader);
+  expect(screen.getAllByTestId("foodItems").length).toBe(14);
+
+  // click on add button for one menu to check if going to cart
+  const addBtn = screen.getAllByRole("button", { name: "Add +" });
+  fireEvent.click(addBtn[0]);
+  expect(screen.getByText(/Cart\s*\(\s*1\s*\)/)).toBeInTheDocument();
+});
