26cccaeacb9f173dcc7b2633616574f447b075dc
Dhruba <gdhruba748+github@gmail.com>
2025-08-01 10:35:49 +0530

    test for filter button

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -21,12 +21,34 @@ it("Should search res list for burger text input ", async () => {
       </BrowserRouter>
     )
   );
+  //before search
   const cardsBeforeSearch = screen.getAllByTestId("resCard");
   expect(cardsBeforeSearch.length).toBe(8);
+  // after search
   const searchBtn = screen.getByRole("button", { name: "Search" });
   const searchInput = screen.getByTestId("searchInput"); // comes from body component
   fireEvent.change(searchInput, { target: { value: "burger" } });
   fireEvent.click(searchBtn);
   const cardsAfterSearch = screen.getAllByTestId("resCard"); // comes from restaurant card
   expect(cardsAfterSearch.length).toBe(1);
+});
+
+it("Should filter top rated restaurant ", async () => {
+  await act(() =>
+    render(
+      <BrowserRouter>
+        <Body />
+      </BrowserRouter>
+    )
+  );
+  // before filtering reasults
+  const cardsBeforeFilter = screen.getAllByTestId("resCard");
+  expect(cardsBeforeFilter.length).toBe(8);
+  // after filtering
+  const filterBtn = screen.getByRole("button", {
+    name: "Top rated restaurant",
+  });
+  fireEvent.click(filterBtn);
+  const cardsAfterFilter = screen.getAllByTestId("resCard"); // comes from restaurant card
+  expect(cardsAfterFilter.length).toBe(2); // only 2 restaurant satiesfies the rating criteria in mock data not the ui [ live api ]
 });
